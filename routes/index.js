14253224563f3b453d8ad6e5e79292e88a32f19b
kolbeypruitt <kolbeypruitt@gmail.com>
2015-11-16 22:43:52 -0700

    moved resort backend resort route to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var conString = "postgres://@localhost/powdr";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/v1/allResorts', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM resorts', function(err, result) {
+      done();
+      res.render('resorts/index', {resorts: result})
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log(result.rows[0]);
+      console.log("connected to powdr database");
+    });
+
+  });
 });
+
 
 module.exports = router;
