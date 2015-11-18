c568b29d98bc168dce8296cac3c26298259acb61
Nick Jones <NRJones0210@gmail.com>
2015-11-18 12:49:13 -0700

    json file, main page and individual resort page wired up with angular

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.use(function(req, res, next){
   next()
 })
 app.use('/', routes);
-// app.use('/leagues', leagues);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
