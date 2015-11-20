6829ebc66d0a660a9a589c2e4135385060e3e9a9
kolbeypruitt <kolbeypruitt@gmail.com>
2015-11-20 02:17:00 -0700

    database query is working for favorites

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var pg = require('pg');
 var conString = "postgres://@localhost/powdr";
 
 router.get('/_=_', function(req, res, next) {
-  console.log(req.user);
+  // console.log(req.user);
   res.json(req.user)
 });
 
@@ -32,18 +32,16 @@ router.post('/addFav', function(req, res, next){
 })
 
 router.get('/isFav/:id', function(req, res, next){
-  console.log(req.params.id);
-  console.log(req.user.facebookId);
+  
   pg.connect(conString, function(err, client, done) {
 
     if (err) {
       return console.error('error fetching client from pool', err);
     }
-    client.query('SELECT * FROM favorites WHERE (facebook_id, resort_id) VALUES($1, $2);',[req.user.facebookId, req.params.id], function(err, result) {
+    client.query('SELECT * FROM favorites WHERE (facebook_id = $1 and resort_id = $2);',[req.user.facebookId, req.params.id], function(err, result) {
       done();
-      console.log(result);
-      // console.log(result.rows[0].length);
-      res.redirect('/' + req.body.resortId)
+      // console.log(result.rows[0]);
+      console.log(result.rows.length);
       if (err) {
         return console.error('error running query', err);
       }
